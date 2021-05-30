f750cb0d85397b1b73348aec26b4648ae1fd90a8
daiyada <shunsuke07l6@gmail.com>
2021-05-30 15:57:45 +0900

    1st-commit

--- config/cfg_manager.py
+++ config/cfg_manager.py
@@ -10,10 +10,10 @@ import os
 from inout.load import Load
 
 class ReadMovie2Img(object):
-    PATH = os.path.join(os.getcwd(), "config", "movie2img.yaml")
+    PATH = os.path.join(os.getcwd(), "config", "movie2img_param.yaml")
 
     @classmethod
-    def getYaml(cls): return cls.PATH
+    def getYamlPath(cls): return cls.PATH
 
     @property
     def getStartTime(self): return self.__start_time
@@ -24,14 +24,19 @@ class ReadMovie2Img(object):
     @property
     def getOutputNumber(self): return self.__output_number
 
+    @property
+    def getCutFlag(self): return self.__cut_flag
+
     def __init__(self, path):
         """constructor"""
         self.__load = Load(path)
-        self.__yaml_data = self.__load.load_yaml()
+        self.__yaml_data = self.__load.loadYaml()
+        self.__deserialize()
 
     def __deserialize(self):
         """yamlファイルからparamを読み取る"""
         self.__start_time = int(self.__yaml_data["time"]["start_time"])
         self.__end_time = int(self.__yaml_data["time"]["end_time"])
         self.__output_number = int(self.__yaml_data["number"]["output_sheets"])
+        self.__cut_flag = bool(int(self.__yaml_data["type"]["cut_flag"]))
 
--- movie2img.py
+++ movie2img.py
@@ -8,18 +8,75 @@
 import os
 
 import cv2
+from tqdm import tqdm
 
-def cutImg(movie_path):
+from config.cfg_manager import ReadMovie2Img
+
+
+def cutIntervally(cap, start_time, end_time, output_number, output_dir, ext="jpg"):
+    """
+    @brief 一定間隔に切り出す
+    """
+    if not start_time < end_time:
+        print("[ERROR]切り出し開始時間と切り出し終了時間の大小関係")
+        raise Exception
+    final_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    start_frame = start_time * fps
+    end_frame = end_time * fps
+    # 切り出し最終フレームの見直し
+    if end_frame > final_frame:
+        end_frame = final_frame
+    # step: 切り出すフレーム間隔
+    step = int((end_frame - start_frame) / output_number)
+    for index, frame_num in enumerate(range(start_frame, end_frame, step), start=1):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+        ret, img = cap.read()   # img: (numpy.ndarray)
+        if ret:
+            cv2.imwrite(os.path.join(output_dir, "{}.{}".format(index, ext)), img)
+        else:
+            break
+
+def cutAll(cap, output_dir, ext="jpg"):
+    """
+    @brief 全フレーム切り出す
+    """
+    index = 1
+    while True:
+        ret, img = cap.read()
+        if ret:
+            cv2.imwrite(os.path.join(output_dir, "{}.{}".format(index, ext)), img)
+            index += 1
+        else:
+            break
+
+def cutImg(movie_path, output_dir, config):
+    """
+    @param movie_path (str) 動画ファイルのパス
+    @param output_dir (str) 出力する画像のdirectory
+    @param config (class) 出力する画像に関するオプション
+    """
     cap = cv2.VideoCapture(movie_path)
-    print("cap: ", cap)
-    print("cap: ", type(cap))
     if not cap.isOpened():
         return
-
-
+    """
+    FIXME: 本当は総フレーム数を取得して、endtime(フレーム番号)が総フレーム数より
+    大きい場合は、end_time = fps * 総フレーム数に書き換える処理を入れたいが、なぜか
+    raspiカメラで撮影したデータは cap.get(cv2.CAP_PROP_FRAME_COUNT)で総フレーム数を取得できない
+    よって現状はこの処理を省いている
+    """
+    if config.getCutFlag:
+        cutAll(cap, output_dir)
+    else:
+        cutIntervally(cap, config.start_time, config.end_time, config.output_number, output_dir)
+    print("DONE")
 
 if __name__ == "__main__":
+    # file_name = "video.avi"
     file_name = "video_20210525_11.avi"
     movie_path = os.path.join(os.getcwd(), "movie", file_name)
-    output_path = os.path.join(os.getcwd(), "calibration_img", file_name)
-    cutImg(movie_path)
+    output_dir = os.path.join(os.getcwd(), "calibration_img")
+
+    config = ReadMovie2Img(ReadMovie2Img.getYamlPath())
+    cutImg(movie_path, output_dir, config)
